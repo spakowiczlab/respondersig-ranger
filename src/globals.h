599bcbbec4ed4d0e8d565ec1e44471b10e66e445
Caroline Dravillas <wheelercaroline073@gmail.com>
2026-07-24 10:59:18 -0400

    add comments

--- src/globals.h
+++ src/globals.h
@@ -7,6 +7,9 @@ This software may be modified and distributed under the terms of the MIT license
 
 Please note that the C++ core of ranger is distributed under MIT license and the
 R package "ranger" under GPL3 license.
+ 
+ Edited by Caroline Dravillas to implement stratified gini index for handling
+ batch effects.
 #-------------------------------------------------------------------------------*/
 
 #ifndef GLOBALS_H_
